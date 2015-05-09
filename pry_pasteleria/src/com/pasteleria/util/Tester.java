1040be181fde750ac521e9b6d672d450c79050e7
leonxandercs <alex_0446@hotmail.com>
2015-05-08 19:25:48 -0500

    commit

--- pry_pasteleria/src/com/pasteleria/util/Tester.java
+++ pry_pasteleria/src/com/pasteleria/util/Tester.java
@@ -1,7 +1,5 @@
 package com.pasteleria.util;
 
-import com.pasteleria.bean.Client;
-import com.pasteleria.bean.Employed;
 import com.pasteleria.bean.User;
 import com.pasteleria.services.UserImplement;
 
@@ -15,6 +13,7 @@ public class Tester {
 	public static void main(String[] args) {
 		UserImplement i=new UserImplement();
 		User o=i.find("E","leonxandercs@gmail.com", "123456");
+		System.out.println(o.getIdUsuario());
 		System.out.println(o.getNombre());
 		System.out.println(o.getRol().getDescripcion());
 		System.out.println(o.getRol().getIdRol());
