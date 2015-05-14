c0ffce7680a1424afba537729558d54ff39e652b
leonxandercs <alex_0446@hotmail.com>
2015-05-14 04:06:53 -0500

    combos-w

--- pry_pasteleria/WebContent/dashboard/test.jsp
+++ pry_pasteleria/WebContent/dashboard/test.jsp
@@ -5,16 +5,36 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 <title>Test Struts2</title>
 </head>
-<body>
+<body bgcolor="yellow">
 <s:textfield label="Greet " name="mensaje"/>
 
-<s:select list="estados" 
+<s:select label="Estados"  list="estados" 
 headerKey="0" headerValue="--Seleccione--" 
-listKey="idEstado" listValue="descripcion" >
+listKey="idEstado" listValue="descripcion"/>
+<br></br>
 
-</s:select>
+<s:select label="Categorias"  list="categorias" 
+headerKey="0" headerValue="--Seleccione--" 
+listKey="idCategoria" listValue="descripcion"/>
+<br></br>
+
+<s:select label="Coberturas"  list="coberturas" 
+headerKey="0" headerValue="--Seleccione--" 
+listKey="idCobertura" listValue="descripcion"/>
+<br></br>
+
+<s:select label="Masas"  list="masas" 
+headerKey="0" headerValue="--Seleccione--" 
+listKey="idMasa" listValue="descripcion"/>
+<br></br>
+
+<s:select label="Rellenos"  list="rellenos" 
+headerKey="0" headerValue="--Seleccione--" 
+listKey="idRelleno" listValue="descripcion"/>
 
 </body>
 </html>
--- pry_pasteleria/src/com/pasteleria/actions/TestAction.java
+++ pry_pasteleria/src/com/pasteleria/actions/TestAction.java
@@ -2,11 +2,20 @@ package com.pasteleria.actions;
 
 import java.util.List;
 
+
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Result;
 
 import com.opensymphony.xwork2.ActionSupport;
+import com.pasteleria.bean.Category;
+import com.pasteleria.bean.Coverage;
+import com.pasteleria.bean.Dough;
+import com.pasteleria.bean.Filling;
 import com.pasteleria.bean.Status;
+import com.pasteleria.services.ServiceCategory;
+import com.pasteleria.services.ServiceCoverage;
+import com.pasteleria.services.ServiceDough;
+import com.pasteleria.services.ServiceFilling;
 import com.pasteleria.services.ServiceStatus;
 
 @SuppressWarnings("serial")
@@ -14,11 +23,19 @@ public class TestAction extends ActionSupport {
 
 	private String mensaje;
 	private List<Status> estados;
+	private List<Category> categorias;
+	private List<Coverage> coberturas;
+	private List<Dough> masas;
+	private List<Filling> rellenos;
 
 	@Action(value = "ListStatus", results = { @Result(name = "success", location = "/dashboard/test.jsp") })
 	public String getListStatus() {
 		ServiceStatus statusImplement=new ServiceStatus();
 		estados =statusImplement.list();
+		categorias=new ServiceCategory().list();
+		coberturas=new ServiceCoverage().list();
+		masas=new ServiceDough().list();
+		rellenos=new ServiceFilling().list();
 		mensaje="Hellow Panther";
 		return SUCCESS;
 	}
@@ -38,6 +55,39 @@ public class TestAction extends ActionSupport {
 	public void setEstados(List<Status> estados) {
 		this.estados = estados;
 	}
+
+	public List<Category> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Category> categorias) {
+		this.categorias = categorias;
+	}
+
+	public List<Coverage> getCoberturas() {
+		return coberturas;
+	}
+
+	public void setCoberturas(List<Coverage> coberturas) {
+		this.coberturas = coberturas;
+	}
+
+	public List<Dough> getMasas() {
+		return masas;
+	}
+
+	public void setMasas(List<Dough> masas) {
+		this.masas = masas;
+	}
+
+	public List<Filling> getRellenos() {
+		return rellenos;
+	}
+
+	public void setRellenos(List<Filling> rellenos) {
+		this.rellenos = rellenos;
+	}
+
 	
-	
+
 }
